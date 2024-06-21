2244e00c52a4b7d0b249014a8a1defbaf422e551
collintegral <collintegral@gmail.com>
2024-06-21 18:46:25 -0400

    Week one of mock chamber site

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,9 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+document.getElementById("lastmod").innerHTML = `Last Modified ${document.lastModified}`;
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
